4babd2449d9dd480dbfcc001899e164538399444
No4vick <8200171@edu.miet.ru>
2022-10-22 17:43:38 +0300

    placeholder answer insert done

--- db_works.py
+++ db_works.py
@@ -12,12 +12,20 @@ client = MongoClient(os.getenv('DB_URL'))
 db = client['autodoor']
 
 
-def create_placeholder_form():
+def insert_placeholder_form():
     collection = db.forms
     with open('placeholder_form.json', 'r', encoding='utf-8') as f:
         placeholder = loads(f.read())
     collection.insert_one(placeholder)
 
 
+def insert_placeholder_dorogi_answer():
+    collection = db.Dorogi
+    with open('placeholder_answer.json', 'r', encoding='utf-8') as f:
+        placeholder = loads(f.read())
+    collection.insert_one(placeholder)
+
+
 if __name__ == '__main__':
-    create_placeholder_form()
+    # insert_placeholder_form()
+    insert_placeholder_dorogi_answer()
